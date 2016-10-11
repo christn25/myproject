c18df9be68180fad6997dc8cd2bf76373c89e78f
Nicolas Christ <nicolas.christ25@gmail.com>
2016-10-11 11:49:46 +0200

    creation du fichier '.gitignore'

---
+++ src/main/java/myproject/Run.java
@@ -0,0 +1,21 @@
+package myproject;
+
+import com.google.common.base.Strings;
+
+public class Run {
+
+	public static void main(String[] args) {
+		System.out.println(Strings.isNullOrEmpty("Yolo"));
+		System.out.println(Strings.isNullOrEmpty(""));
+		System.out.println(Strings.isNullOrEmpty(null));
+		System.out.println("Yep");
+		System.out.println("Yep");
+		System.out.println("Yep");
+		System.out.println("Yep");
+		System.out.println("Yep");
+		System.out.println("Yep");
+		System.out.println("Yep");
+		System.out.println("Mais pourquoi tu dis 'yep' ??");
+		System.out.println("Parce que j'ai envi de dire 'yep'!");
+	}
+}
