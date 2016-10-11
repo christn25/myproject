f9131547c0bea54063f59b14220832c666244f04
Nicolas Soriano <nico.sor@free.fr>
2016-10-11 15:07:14 +0200

    Oh! Une licorne !

--- src/main/java/myproject/Run.java
+++ src/main/java/myproject/Run.java
@@ -1,5 +1,10 @@
 package myproject;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 //import com.google.common.base.Strings;
 
 public class Run {
@@ -20,6 +25,25 @@ public class Run {
 		System.out.println("Parce que j'ai envi de dire 'yep'!");
 		System.out.println("Et je vous déconseille la mousse au chocolat du patron");
 		System.out.println("Cookiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiies !!!");
+		
+		String chaine="";
+		String fichier ="fichiertexte.txt";
+		
+		//lecture du fichier texte	
+		try{
+			InputStream ips=new FileInputStream(fichier); 
+			InputStreamReader ipsr=new InputStreamReader(ips);
+			BufferedReader br=new BufferedReader(ipsr);
+			String ligne;
+			while ((ligne=br.readLine())!=null){
+				System.out.println(ligne);
+				chaine+=ligne+"\n";
+			}
+			br.close(); 
+		}		
+		catch (Exception e){
+			System.out.println(e.toString());
+		}  
 	}
 }
 
