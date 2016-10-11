f388c5cd66491e66fc056677c17527b0ad0ae13d
Pierre B <pierreburgain@gmail.com>
2016-10-11 14:28:26 +0200

    commenté ce qui fait que ca marche pas pour moi

--- src/main/java/myproject/Run.java
+++ src/main/java/myproject/Run.java
@@ -1,13 +1,13 @@
 package myproject;
 
-import com.google.common.base.Strings;
+//import com.google.common.base.Strings;
 
 public class Run {
 
 	public static void main(String[] args) {
-		System.out.println(Strings.isNullOrEmpty("Yolo"));
+		/*System.out.println(Strings.isNullOrEmpty("Yolo"));
 		System.out.println(Strings.isNullOrEmpty(""));
-		System.out.println(Strings.isNullOrEmpty(null));
+		System.out.println(Strings.isNullOrEmpty(null));*/
 		System.out.println("Yep");
 		System.out.println("Yep");
 		System.out.println("Yep");
@@ -20,3 +20,5 @@ public class Run {
 		System.out.println("Et je vous déconseille la mousse au chocolat du patron");
 	}
 }
+
+//PRESENT !!!!!!!!!!!!!!!!!!
